b76adfb823e9439b8ef5209d8f312f962868731e
hussein-sultan <you@example.com>
2026-02-11 02:56:07 +0300

    feat: Update API proxy target to production server

--- vite.config.js
+++ vite.config.js
@@ -31,7 +31,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://ec2-13-60-105-141.eu-north-1.compute.amazonaws.com:8080',
         changeOrigin: true,
         secure: false,
         ws: true,
